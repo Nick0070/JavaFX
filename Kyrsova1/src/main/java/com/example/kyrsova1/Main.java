0993a66b5a354485982278063c9037ee537f607a
Nick <64158017+Nick0070@users.noreply.github.com>
2023-12-21 15:52:02 +0300

    add check pass and login

--- Kyrsova1/src/main/java/com/example/kyrsova1/Main.java
+++ Kyrsova1/src/main/java/com/example/kyrsova1/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 867, 500);
 
         primaryStage.setTitle("Автомастерская");
--- Kyrsova1/src/main/java/com/example/kyrsova1/login.java
+++ Kyrsova1/src/main/java/com/example/kyrsova1/login.java
@@ -1,4 +1,45 @@
+
 package com.example.kyrsova1;
 
+    import javafx.fxml.FXML;
+    import javafx.stage.Stage;
+    import javafx.scene.Scene;
+    import java.io.IOException;
+    import javafx.scene.Parent;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.control.Button;
+    import javafx.scene.control.TextField;
+
 public class login {
+    @FXML
+    private Button loginbutton;
+    @FXML
+    private TextField logintextfield;
+    @FXML
+    private TextField passtextfield;
+    @FXML
+    void initialize() {
+
+        loginbutton.setOnAction(event -> {
+            String enteredLogin = logintextfield.getText().trim();
+            String enteredPassword = passtextfield.getText().trim();
+
+            if ("admin".equals(enteredLogin) && "admin".equals(enteredPassword)) {
+                loginbutton.getScene().getWindow().hide();
+
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("Main.fxml"));
+
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Parent root = loader.getRoot();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+            }
+        });
+    }
 }
