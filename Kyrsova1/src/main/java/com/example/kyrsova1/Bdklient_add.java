09d8034a752651a2e8e8deb72800a94751ac6841
Nick <64158017+Nick0070@users.noreply.github.com>
2023-12-22 20:44:05 +0300

    add back windows

--- Kyrsova1/src/main/java/com/example/kyrsova1/Bdklient_add.java
+++ Kyrsova1/src/main/java/com/example/kyrsova1/Bdklient_add.java
@@ -1,4 +1,34 @@
 package com.example.kyrsova1;
 
+    import javafx.fxml.FXML;
+    import javafx.scene.Scene;
+    import javafx.stage.Stage;
+    import java.io.IOException;
+    import javafx.scene.Parent;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.control.Button;
+
 public class Bdklient_add {
+
+    @FXML
+    private Button Button_cancel;
+
+    @FXML
+    void initialize() {
+        ///Кнопка возврата назад
+        Button_cancel.setOnAction(event -> {
+            Button_cancel.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Bdklient.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+    }
 }
--- Kyrsova1/src/main/java/com/example/kyrsova1/BdrepairsAdd.java
+++ Kyrsova1/src/main/java/com/example/kyrsova1/BdrepairsAdd.java
@@ -1,4 +1,34 @@
 package com.example.kyrsova1;
 
+    import javafx.fxml.FXML;
+    import javafx.scene.Scene;
+    import javafx.stage.Stage;
+    import java.io.IOException;
+    import javafx.scene.Parent;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.control.Button;
+
 public class BdrepairsAdd {
+
+    @FXML
+    private Button Button_cancel2;
+    @FXML
+    void initialize() {
+
+        ///Кнопка возврата назад
+        Button_cancel2.setOnAction(event -> {
+            Button_cancel2.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Bdrepairs.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+    }
 }
